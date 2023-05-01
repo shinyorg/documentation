6cb98fd13e760f93788eaee22739175c79b8d530
Allan Ritchie <allan.ritchie@gmail.com>
2023-05-01 09:43:41 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shinylib.net',
+  site: 'https://shinylib.net/documentation',
   markdown: { drafts: true },
   integrations: [
     tailwind(), 
