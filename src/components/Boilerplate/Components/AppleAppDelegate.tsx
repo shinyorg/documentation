fa9afd9e53f2025190472461529b6c926668be2b
Allan Ritchie <allan.ritchie@gmail.com>
2023-05-21 12:06:33 -0400

    Update AppleAppDelegate.tsx

--- src/components/Boilerplate/Components/AppleAppDelegate.tsx
+++ src/components/Boilerplate/Components/AppleAppDelegate.tsx
@@ -14,15 +14,15 @@ const AppleAppDelegate = () => {
   
       [Export("application:didRegisterForRemoteNotificationsWithDeviceToken:")]
       public void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
-          => global::Shiny.Hosting.Host.Current.Lifecycle().OnRegisteredForRemoteNotifications(deviceToken);
+          => global::Shiny.Hosting.Host.Lifecycle.OnRegisteredForRemoteNotifications(deviceToken);
   
       [Export("application:didFailToRegisterForRemoteNotificationsWithError:")]
       public void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
-          => global::Shiny.Hosting.Host.Current.Lifecycle().OnFailedToRegisterForRemoteNotifications(error);
+          => global::Shiny.Hosting.Host.Lifecycle.OnFailedToRegisterForRemoteNotifications(error);
   
       [Export("application:didReceiveRemoteNotification:fetchCompletionHandler:")]
       public void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
-          => global::Shiny.Hosting.Host.Current.Lifecycle().OnDidReceiveRemoveNotification(userInfo, completionHandler);	
+          => global::Shiny.Hosting.Host.Lifecycle.OnDidReceiveRemoveNotification(userInfo, completionHandler);	
   
   }`;
   return (<Syntax source={src} />);
