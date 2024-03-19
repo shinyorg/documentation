a0bc7f3d28ab9ff376d35aafbadf7fef8d1678f8
Allan Ritchie <allan.ritchie@gmail.com>
2024-03-19 19:23:17 -0400

    Fix up release notes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import react from '@astrojs/react';
 import starlightBlog from 'starlight-blog';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [
     react(),
+    tailwind(),
     starlight({
       title: 'Shiny.NET',
       // tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 2 },
