fa8f73708ac910a23307078ad81189c9c60a232f
Allan Ritchie <allan.ritchie@gmail.com>
2024-03-24 17:18:54 -0400

    Updates

--- src/components/Boilerplate/Components/ProjectFile.tsx
+++ src/components/Boilerplate/Components/ProjectFile.tsx
@@ -18,12 +18,15 @@ const ProjectFile = (props: Props) => {
             (a.nuget > b.nuget) ? 1 : ((b.nuget > a.nuget) ? -1 : 0))
         );
 
+    const hasNotifications = nugets.find(x => x.nuget === "Shiny.Notifications") !== undefined;
+    const hasPush = usesPush(props.components);
+
     nugets = [...nugets, { id: "hosting", nuget: "Shiny.Hosting.Maui", version: DEFAULT_VERSION } as ShinyComponent];
     let pr = "<ItemGroup>\r\n";
     nugets.map(c => {
         pr += `\t<PackageReference Include="${c.nuget}" version="${c.version}" />\r\n`;
     });
-    pr += "</ItemGroup>";
+    pr += "</ItemGroup>\r\n";
 
     if (nugets.find(x => x.nuget === "Shiny.Extensions.Configuration") !== undefined) {
         pr += "\r\n<ItemGroup>\r\n";
@@ -33,19 +36,25 @@ const ProjectFile = (props: Props) => {
         pr += "\t<MauiAsset Include=\"appsettings.ios.json\" LogicalName=\"appsettings.ios.json\" />\r\n";
         pr += "\t<MauiAsset Include=\"appsettings.maccatalyst.json\" LogicalName=\"appsettings.maccatalyst.json\" />\r\n";
         pr += "\t<MauiAsset Include=\"appsettings.android.json\" LogicalName=\"appsettings.android.json\" />\r\n";
-        pr += "</ItemGroup>";
+        pr += "</ItemGroup>\r\n";
     }
 
-    // <!--
-    // For scheduled notifications, you need to setup "Time Sensitive Notifications" in the Apple Developer Portal for your app provisioning and uncomment below
-    // <CustomEntitlements Include="com.apple.developer.usernotifications.time-sensitive" Type="Boolean" Value="true" />
-    // -->    
-    if (usesPush(props.components)) {
+    if (hasNotifications || hasPush) {
         pr += "\r\n<ItemGroup Condition=\"$(TargetFramework.Contains('-ios'))\">\r\n";
-        pr += "\t<CustomEntitlements Include=\"aps-environment\" Type\"string\" Value=\"development\" Condition=\"'$(Configuration)' == 'Debug'\" />\r\n"
-        pr += "\t<CustomEntitlements Include=\"aps-environment\" Type\"string\" Value=\"production\" Condition=\"'$(Configuration)' == 'Release'\" />\r\n"
+        if (hasNotifications) {
+            pr += "\t<!--\r\n";
+            pr += "\t// For scheduled notifications, you need to setup \"Time Sensitive Notifications\" in the Apple Developer Portal for your app provisioning and uncomment below\r\n";
+            pr += "\t// <CustomEntitlements Include=\"com.apple.developer.usernotifications.time-sensitive\" Type=\"Boolean\" Value=\"true\" />\r\n";
+            pr += "\t-->\r\n";
+        }
+        if (hasPush) {
+            pr += "\t<CustomEntitlements Include=\"aps-environment\" Type\"string\" Value=\"development\" Condition=\"'$(Configuration)' == 'Debug'\" />\r\n"
+            pr += "\t<CustomEntitlements Include=\"aps-environment\" Type\"string\" Value=\"production\" Condition=\"'$(Configuration)' == 'Release'\" />\r\n"
+        }
         pr += "</ItemGroup>";
     }
+  
+
 
     return (<Syntax source={pr} language="xml" />);
 }
