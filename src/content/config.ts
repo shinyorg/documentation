a097c1e481c9c1f4491964e8c70a433893151cd6
Allan Ritchie <allan.ritchie@gmail.com>
2024-06-19 21:56:55 -0400

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,11 @@ import { docsSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'starlight-blog/schema'
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema({ extend: blogSchema() }) })
+  docs: defineCollection({ 
+    schema: docsSchema({ 
+      extend: (context) => blogSchema(context)
+    }) 
+  })
   // docs: defineCollection({ schema: docsSchema() }),
   // i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
