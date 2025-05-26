95fb16fb680cbef4a6e8b5d6231443fb07c5e3e7
Allan Ritchie <allan.ritchie@gmail.com>
2025-05-25 23:03:12 -0400

    Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -193,8 +193,7 @@ export default defineConfig({
                   { label: 'ASP.NET Core', link: 'mediator/extensions/aspnet' },
                   { label: 'HTTP', link: 'mediator/extensions/http' },
                   { label: 'Prism', link: 'mediator/extensions/prism' },
-                  { label: 'Dapper', link: 'mediator/extensions/dapper' },
-                  { label: 'Sentry', link: 'mediator/extensions/sentry' },
+                  { label: 'Dapper', link: 'mediator/extensions/dapper' }
                 ]
             },
           ]
