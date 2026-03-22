9e61b80435b0169cb5fbb6864a285c8cdde23005
Allan Ritchie <allan.ritchie@gmail.com>
2026-03-22 19:47:36 -0400

    Fix

--- src/components/NugetBadge.tsx
+++ src/components/NugetBadge.tsx
@@ -14,6 +14,6 @@ const NugetBadge = (props: Props) => {
         imgUrl += `&label=${props.name}`;
     }
     const nugetUrl: string = `https://www.nuget.org/packages/${props.name}`;
-    return (<span style={{ display: 'inline-flex', alignItems: 'center', verticalAlign: 'middle' }}><a href={nugetUrl} target="_NEWWINDOW"><img src={imgUrl} /></a></span>);
+    return (<span style={{ display: 'inline-flex', alignItems: 'center', verticalAlign: 'middle' }}><a href={nugetUrl} target="_blank" rel="noopener noreferrer"><img src={imgUrl} alt={`NuGet package ${props.name}`} /></a></span>);
 };
 export default NugetBadge;
